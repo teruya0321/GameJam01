af2341de6820cc222feb510aa028a876c3e30860
TsubasaMaezato <c22024@std.it-college.ac.jp>
2023-03-02 18:07:03 +0900

    敵が回転しちゃうのと、壁越しでも認識できるのをどうにかしなきゃ

--- GameJam01/Assets/Scripts/TsubasaScripts/TsubasaScriptEnemy.cs
+++ GameJam01/Assets/Scripts/TsubasaScripts/TsubasaScriptEnemy.cs
@@ -2,31 +2,78 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.AI;
 
 public class TsubasaScriptEnemy : MonoBehaviour
 {
+    public GameObject player;
+
+    private NavMeshAgent navMeshAgent;
+
     Ray ray;
     RaycastHit hit;
 
-    public int distance; //ray‹——£
+    private float chargeTime;
+    private float timeCount;
+
+    public float force;
+    public float distance;
+
+
+    private bool moveEnemy;
     void Start()
     {
+        navMeshAgent = GetComponent<NavMeshAgent>(); //NavMeshAgent‚ð•ÛŽ
+        moveEnemy = true;
 
+        chargeTime = 3;
     }
     void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward * 1.0f,Color.red);
+        //rayPos = transform.position + new Vector3(0, 0, -8);  //Ray‚ÌˆÊ’u
+        ray = new Ray(transform.position,transform.forward * distance);
+        Debug.DrawRay(transform.position, transform.forward * distance,Color.red);
 
-        if (Physics.SphereCast(transform.position, 1.0f, transform.forward, out hit, 5.0f,LayerMask.GetMask("Player")))
+        if (Physics.Raycast(ray,out hit,distance))
         {
-            Debug.Log("Player‚É“–‚½‚Á‚½");
+            if (hit.collider.CompareTag("Player"))
+            {
+                moveEnemy = false;
+                Debug.Log("“–‚½‚Á‚½");
+                navMeshAgent.destination = player.transform.position;
+            }
+            else
+            {
+                moveEnemy = true;
+            }
+        }
+        if (moveEnemy)
+        {
+            Debug.Log("’T‚µ’†");
+
+            timeCount += Time.deltaTime;
+            transform.position += transform.forward * Time.deltaTime;
+
+            if (timeCount > chargeTime)
+            {
+                Vector3 course = new Vector3(0, UnityEngine.Random.Range(0, 360), 0);
+                transform.localRotation = Quaternion.Euler(course);
+
+                timeCount = 0;
+            }
         }
     }
-    void OnDrawGizmos()
+    public void OnDestectObject(Collider collider)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position + transform.forward * 1.0f, 1.0f);
+        if (collider.CompareTag("Player"))
+        {
+            navMeshAgent.destination = collider.transform.position;
+            Debug.Log("’Ç‚¢‚©‚¯");
+        }
+        else
+        {
+            moveEnemy = true;
+        }
     }
 
 }
