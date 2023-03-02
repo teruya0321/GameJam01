def1dcee81dab039c5be1edac174ff872601114f
TsubasaMaezato <c22024@std.it-college.ac.jp>
2023-03-02 15:18:38 +0900

    敵の追跡とraycast仮導入しました。

---
+++ GameJam01/Assets/Scripts/TsubasaScripts/CollisionDetector.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionDetector : MonoBehaviour
+{
+    [SerializeField] private TriggerEvent onTriggerStay = new TriggerEvent();
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        onTriggerStay.Invoke(other);
+    }
+
+    [Serializable]
+    public class TriggerEvent : UnityEvent<Collider>
+    {
+    }
+}
